3bf57c7da86b30f730635d0b53d41295d46f887d
Zach Freedman <zfreedman92@gmail.com>
2018-06-28 14:18:15 -0400

    feat: Removed <Badge /> from project (wasn't part of work)

--- app/index.js
+++ app/index.js
@@ -2,17 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom";
 require("./index.css");
 
-import Badge from "./badge";
-
 class App extends React.Component {
   render() {
     return (
       <div>
-        <Badge
-          img="https://avatars0.githubusercontent.com/u/2933430?v=36s=460"
-          name="Tyler Mcginnis"
-          username="tylermcginnis"
-        />
+        Sup world
       </div>
     );
   }
